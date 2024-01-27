ee24691a87858395bea09efb5c58c967fbb096a2
Matt Jewell <m@mattjewell.co.uk>
2024-01-27 17:12:29 +0100

    added mason and lsp

--- lua/mattisfrommars/keymap.lua
+++ lua/mattisfrommars/keymap.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>pt", ":Neotree toggle<CR>", {})
+
+-- Window/Paging
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,4 +6,27 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				window = {
+					mappings = { o = "system_open" },
+				},
+				commands = { system_open = function(state)
+					local node = state.tree:get_node()
+					local path = node:get_id()
+					vim.fn.jobstart(
+						{ "xdg-open", "-g", path },
+						{ detach = true }
+					)
+				end },
+			},
+			event_handlers = { {
+				event = "file_opened",
+				handler = function(file_path)
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			} },
+		})
+	end,
 }

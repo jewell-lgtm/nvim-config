a4c9460acae1ec6c43682327dfdd43f4506dded1
Matt Jewell <m@mattjewell.co.uk>
2024-02-04 14:35:04 +0100

    added astro lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,75 +1,79 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true
-    }
-	},
-	{
-		"neovim/nvim-lspconfig",
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
     lazy = false,
-		config = function()
-			local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+    config = function()
+      local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
+      require("mason").setup({})
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
           "tailwindcss",
-					"kotlin_language_server",
-				},
-			})
-			require("lspconfig").lua_ls.setup({
-				capabilities = cmp_capabilities,
-			})
-			require("lspconfig").tsserver.setup({
-				capabilities = cmp_capabilities,
-			})
-			require("lspconfig").kotlin_language_server.setup({
-				capabilities = cmp_capabilities,
-			})
-			require("lspconfig").tailwindcss.setup({
-        capabilities = cmp_capabilities
+          "kotlin_language_server",
+          "astro",
+        },
       })
-			-- Global mappings.
-			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+      require("lspconfig").lua_ls.setup({
+        capabilities = cmp_capabilities,
+      })
+      require("lspconfig").tsserver.setup({
+        capabilities = cmp_capabilities,
+      })
+      require("lspconfig").kotlin_language_server.setup({
+        capabilities = cmp_capabilities,
+      })
+      require("lspconfig").tailwindcss.setup({
+        capabilities = cmp_capabilities,
+      })
+      require("lspconfig").astro.setup({
+        capabilities = cmp_capabilities,
+      })
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-					-- vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+          -- vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
           -- renaming is done in rename.lua
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				end,
-			})
-		end,
-	},
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        end,
+      })
+    end,
+  },
 }

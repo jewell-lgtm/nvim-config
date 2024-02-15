e40c909940d844eb191c5343c748b27b00b9223d
Matt Jewell <m@mattjewell.co.uk>
2024-02-15 08:35:28 +0100

    added todo

--- lua/mattisfrommars/keymap.lua
+++ lua/mattisfrommars/keymap.lua
@@ -1,10 +1,10 @@
-local builtin = require("telescope.builtin")
+
 
 -- Project (view,file,grep,tree) type commands
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>pt", ":Neotree toggle<CR>", {})
+-- vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+-- vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+-- vim.keymap.set("n", "<leader>pt", ":Neotree toggle<CR>", {})
 
 -- Window/Paging
 vim.keymap.set("n", "<C-d>", "<C-d>zz")

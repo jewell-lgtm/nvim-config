3da644b622651e106b3167675dc8acb0ded49acc
Matt Jewell <m@mattjewell.co.uk>
2024-02-22 08:59:47 +0100

    use the neovim kickstart

---
+++ README.md
@@ -0,0 +1,4 @@
+# Matt's Neovim Setup
+
+[Kickstarted](https://github.com/nvim-lua) because aint nobody got time for that.
+
--- lua/mattisfrommars/init.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-
-require("mattisfrommars.lazy")
-require("mattisfrommars.options")
-require("mattisfrommars.keymap")

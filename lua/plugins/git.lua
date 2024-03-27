b4ecbb900d23969c49ab65125d572618ccb3f9d7
Matt Jewell <matt.jewell@pauilina24.de>
2024-03-27 10:57:41 +0100

    added some stuff

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -83,16 +83,4 @@ return {
 
     config = function() end,
   },
-
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      'nvim-telescope/telescope.nvim', -- optional
-    },
-    config = true,
-  },
 }

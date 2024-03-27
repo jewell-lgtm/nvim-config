e9fae72994d411f145a862a86695affde6431158
Matt Jewell <m@mattjewell.co.uk>
2024-03-27 22:05:55 +0100

    colors and stuff

--- init.lua
+++ init.lua
@@ -744,6 +744,10 @@ require('lazy').setup({
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Normal guibg=NONE ctermbg=NONE'
+      vim.cmd.hi 'NormalNC guibg=NONE ctermbg=NONE'
+      vim.cmd.hi 'TelescopeBorder guibg=NONE'
+      vim.cmd.hi 'NvimTreeNormal guibg=NONE'
     end,
   },
 

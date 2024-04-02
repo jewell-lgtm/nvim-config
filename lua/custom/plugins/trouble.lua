431e575bf82a891e3f740ba859e062e900831162
Matt Jewell <m@mattjewell.co.uk>
2024-04-02 11:26:43 +0200

    added trouble, and search git changes

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'folke/trouble.nvim',
+    branch = 'dev',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+    },
+    opts = {},
+  },
+}

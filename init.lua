2a894ab4af850d8b20229e78ee399cc5c054e6dd
Matt Jewell <m@mattjewell.co.uk>
2024-06-26 13:48:18 +0200

    some simple setup that mostly works

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+require("config.options")
 require("config.lazy")
+require("config.colorscheme")
+
+-- Set a transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
